6fb40e7d510bb20d0f6b2c14baad5e5592dddb37
4ubiks <jaharris20@liberty.edu>
2026-05-22 11:14:09 -0400

    added board initialization function

--- skyinit.c
+++ skyinit.c
@@ -1,6 +1,7 @@
 #include "constants.h"
 #include "skyinit.h"
 #include "player.h"
+#include "board.h"
 
 struct Player initializePlayer(struct Player player){
     player.stone_1 = ARKEYAN_BOMBER;
@@ -12,4 +13,21 @@ struct Player initializePlayer(struct Player player){
     player.points = 0;
 
     return player;
+}
+
+struct Board initializeBoard(struct Board board){
+
+    board.piecesPlayed = 0;
+
+    board.point00 = '0';
+    board.point01 = '0';
+    board.point02 = '0';
+    board.point10 = '0';
+    board.point11 = '0';
+    board.point12 = '0';
+    board.point20 = '0';
+    board.point21 = '0';
+    board.point22 = '0';
+
+    return board;
 }
