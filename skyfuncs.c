88549612b4243285db7e017669cb7567378e990e
4ubiks <jaharris20@liberty.edu>
2026-05-22 12:25:07 -0400

    new source file just dropped

---
+++ skyfuncs.c
@@ -0,0 +1,12 @@
+// c includes
+
+// custom includes
+#include "board.h"
+#include "skyfuncs.h"
+
+char setPiece(struct Board *board, int pieceNumber){
+    //board.piece
+    board->boardPieceIsPresent[pieceNumber] = '1';
+
+    return '1';
+}
