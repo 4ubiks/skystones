3dafb70e34851d581bf16a6f646b6d04316f91bb
4ubiks <jaharris20@liberty.edu>
2026-05-22 12:24:15 -0400

    restructured board implementation

--- board.h
+++ board.h
@@ -5,18 +5,8 @@
 struct Board {
     int piecesPlayed;
 
-    // These points are the board themselves, whether or not the piece is played. '0' is empty, '1' is played. 
-    char point00;
-    char point01;
-    char point02;
-
-    char point10;
-    char point11;
-    char point12;
-    
-    char point20;
-    char point21;
-    char point22;
+    // These points are the board themselves, whether or not a piece exists at that point. 
+    char boardPieceIsPresent[9];
 };
 
 #endif
