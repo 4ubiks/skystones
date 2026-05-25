8c86f08798593d0298333d1019e6664c8ac85c9f
4ubiks <jaharris20@liberty.edu>
2026-05-25 14:53:11 -0400

    i am so stupid

--- skyfuncs.c
+++ skyfuncs.c
@@ -1,5 +1,4 @@
 // c includes
-#include "ncurses.h"
 
 // custom includes
 #include "board.h"
@@ -7,8 +6,7 @@
 
 char setPiece(struct Board *board, int pieceNumber){
     //board.piece
-    mvwprintw(stdscr, 20, 14, "setting piece number %d to 1", pieceNumber);
-    board->boardPieceIsPresent[pieceNumber] = '1';
+   board->boardPieceIsPresent[pieceNumber] = '1';
 
     return '1';
 }
