e811ca159d936fedcaa527a33727a63e46badfc6
4ubiks <jaharris20@liberty.edu>
2026-05-25 16:50:17 -0400

    new function header `pieceStatus`

--- skyinit.h
+++ skyinit.h
@@ -1,3 +1,4 @@
 // initializes stuff
 struct Player initializePlayer(struct Player, int teamNumber);
 struct Board initializeBoard(struct Board board);
+int pieceStatus(struct Board board, int pieceNumber);
