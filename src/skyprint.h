2cc492b8e353d51b8d1268c92ac4bcf329c34c4c
4ubiks <jaharris20@liberty.edu>
2026-06-10 17:25:01 -0400

    removed unnecessary functions that i'll probably add back in later

--- src/skyprint.h
+++ src/skyprint.h
@@ -4,10 +4,6 @@ void printFullBoard(struct Board, int turn);
 
 void printPieceCoordinates(int grid_x, int grid_y);
 
-void printPlayerDeck(struct Player);
+void printPlayerDeck(struct Player player, struct Pieces deckPieces);
 
 void enterPiece(int x, int y);
-
-void printDeckSkystone(char* stoneGraphic[6][12], int x, int y, WINDOW* currentPieceWindow);
-
-void printBoardSkystone(char* stoneGraphic[12][21], int x, int y, WINDOW* currentPieceWindow);
