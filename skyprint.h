9017a0c84910258299b920ff83f79b867dc6ab46
4ubiks <jaharris20@liberty.edu>
2026-06-10 16:01:29 -0400

    two new print functions to print stuff to deck and to board

--- skyprint.h
+++ skyprint.h
@@ -7,3 +7,7 @@ void printPieceCoordinates(int grid_x, int grid_y);
 void printPlayerDeck(struct Player);
 
 void enterPiece(int x, int y);
+
+void printDeckSkystone(char* stoneGraphic[6][12], int x, int y, WINDOW* currentPieceWindow);
+
+void printBoardSkystone(char* stoneGraphic[12][21], int x, int y, WINDOW* currentPieceWindow);
