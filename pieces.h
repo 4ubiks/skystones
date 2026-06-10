fd62b408a0b2834b435fcbb481b9cef3f0eed3b9
4ubiks <jaharris20@liberty.edu>
2026-06-10 16:01:00 -0400

    reformatted piece declarations

--- pieces.h
+++ pieces.h
@@ -4,37 +4,37 @@
 #define PIECES_H
 
 struct Pieces {
-    char deck_grenade_general[6];
+    char deck_grenade_general[6][12];
 
-    char deck_arkeyan_bomber[6];
+    char deck_arkeyan_bomber[6][12];
 
-    char deck_root_runner[6];
+    char deck_root_runner[6][12];
 
-    char deck_crystal_golem[6];
+    char deck_crystal_golem[6][12];
 
-    char deck_blaze_brewer[6];
+    char deck_blaze_brewer[6][12];
 
-    char deck_conquertron[6];
+    char deck_conquertron[6][12];
 
-    char deck_dragonet[6];
+    char deck_dragonet[6][12];
 
-    char deck_d_riveter[6];
+    char deck_d_riveter[6][12];
 
-    char board_grenade_general[6];
+    char board_grenade_general[12][21];
 
-    char board_arkeyan_bomber[6];
+    char board_arkeyan_bomber[12][21];
 
-    char board_root_runner[6];
+    char board_root_runner[12][21];
 
-    char board_crystal_golem[6];
+    char board_crystal_golem[12][21];
 
-    char board_blaze_brewer[6];
+    char board_blaze_brewer[12][21];
 
-    char board_conquertron[6];
+    char board_conquertron[12][21];
 
-    char board_dragonet[6];
+    char board_dragonet[12][21];
 
-    char board_d_riveter[6];
+    char board_d_riveter[12][21];
 };
 
 struct Pieces initializeDeckPieces(struct Pieces);
