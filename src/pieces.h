0639592144cb4aee0a593e072b419e6475e4a356
4ubiks <jaharris20@liberty.edu>
2026-06-30 15:38:17 -0400

    added chars to note number of spikes on each side of a piece

--- src/pieces.h
+++ src/pieces.h
@@ -35,6 +35,52 @@ struct Pieces {
     char board_dragonet[12][21];
 
     char board_d_riveter[12][21];
+
+    // Number of spikes in each respective direction of the piece
+    // A: UP
+    // B: RIGHT
+    // C: DOWN
+    // D: LEFT
+    char GRENADE_GENERAL_A;
+    char GRENADE_GENERAL_B;
+    char GRENADE_GENERAL_C;
+    char GRENADE_GENERAL_D;
+
+    char ARKEYAN_BOMBER_A;
+    char ARKEYAN_BOMBER_B;
+    char ARKEYAN_BOMBER_C;
+    char ARKEYAN_BOMBER_D;
+
+    char ROOT_RUNNER_A;
+    char ROOT_RUNNER_B;
+    char ROOT_RUNNER_C;
+    char ROOT_RUNNER_D;
+
+    char CRYSTAL_GOLEM_A;
+    char CRYSTAL_GOLEM_B;
+    char CRYSTAL_GOLEM_C;
+    char CRYSTAL_GOLEM_D;
+
+    char BLAZE_BREWER_A;
+    char BLAZE_BREWER_B;
+    char BLAZE_BREWER_C;
+    char BLAZE_BREWER_D;
+
+    char CONQUERTRON_A;
+    char CONQUERTRON_B;
+    char CONQUERTRON_C;
+    char CONQUERTRON_D;
+
+    char DRAGONET_A;
+    char DRAGONET_B;
+    char DRAGONET_C;
+    char DRAGONET_D;
+
+    char D_RIVETER_A;
+    char D_RIVETER_A;
+    char D_RIVETER_A;
+    char D_RIVETER_A;
+
 };
 
 struct Pieces initializeDeckPieces(struct Pieces);
