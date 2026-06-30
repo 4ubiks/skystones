0c26b4a885c0f424ab26247101d042fbffb111e8
4ubiks <jaharris20@liberty.edu>
2026-06-30 16:32:20 -0400

    new function header and one old function header change

--- src/skyfuncs.h
+++ src/skyfuncs.h
@@ -7,8 +7,10 @@ char gameIsOver(struct Board*);
 
 int toggleTurn(int turn);
 
-void selectPlayerColor(int tmp_x, int tmp_y, WINDOW* newPieceWindow1, WINDOW* newPieceWindow, struct Board board, int piece_count);
+void selectPlayerColor(int tmp_x, int tmp_y, WINDOW* newPieceWindow1, WINDOW* newPieceWindow, struct Board board, int piece_count, const char* piecePlayed);
 
 int playerScore(struct Board board);
 
 void pickDeckSkystone(WINDOW* currentPieceWindow, int deckWall, struct Pieces deckPieces, int deckPiece);
+
+char* setDeckPieceName(int selectedPiece);
