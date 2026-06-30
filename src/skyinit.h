391b5bf9ecf266db3ea3f89b1bbe12df00178771
4ubiks <jaharris20@liberty.edu>
2026-06-30 15:38:49 -0400

    added initPlayer2 header

--- src/skyinit.h
+++ src/skyinit.h
@@ -1,4 +1,5 @@
 // initializes stuff
-struct Player initializePlayer(struct Player, int teamNumber);
+struct Player initializePlayer1(struct Player, int teamNumber);
+struct Player initializePlayer2(struct Player, int teamNumber);
 struct Board initializeBoard(struct Board board);
 int pieceStatus(struct Board board, int pieceNumber);
