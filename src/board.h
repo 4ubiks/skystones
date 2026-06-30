77b993186f726e2feec809585fe1b3153064c96c
4ubiks <jaharris20@liberty.edu>
2026-06-30 16:31:13 -0400

    adding array to hold each name of piece played

--- src/board.h
+++ src/board.h
@@ -8,6 +8,9 @@ struct Board {
     // These points are the board themselves, whether or not a piece exists at that point. 
     char boardPieceIsPresent[9];
     char boardPiecePlayer[9];
+
+    // holds the name of each piece played at each spot on the 3x3 grid
+    char boardPiecesPlayed[9];
 };
 
 #endif
