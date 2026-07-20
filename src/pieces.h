4ab5a76ece0e2ebc412c0e2aa24155c794e3a9b0
4ubiks <jaharris20@liberty.edu>
2026-07-20 12:11:02 -0400

    making spike numbers pointers

--- src/pieces.h
+++ src/pieces.h
@@ -47,50 +47,52 @@ struct Pieces {
     // B: RIGHT
     // C: DOWN
     // D: LEFT
-    char GRENADE_GENERAL_A;
-    char GRENADE_GENERAL_B;
-    char GRENADE_GENERAL_C;
-    char GRENADE_GENERAL_D;
+    char* GRENADE_GENERAL_A;
+    char* GRENADE_GENERAL_B;
+    char* GRENADE_GENERAL_C;
+    char* GRENADE_GENERAL_D;
 
-    char ARKEYAN_BOMBER_A;
-    char ARKEYAN_BOMBER_B;
-    char ARKEYAN_BOMBER_C;
-    char ARKEYAN_BOMBER_D;
+    char* ARKEYAN_BOMBER_A;
+    char* ARKEYAN_BOMBER_B;
+    char* ARKEYAN_BOMBER_C;
+    char* ARKEYAN_BOMBER_D;
 
-    char ROOT_RUNNER_A;
-    char ROOT_RUNNER_B;
-    char ROOT_RUNNER_C;
-    char ROOT_RUNNER_D;
+    char* ROOT_RUNNER_A;
+    char* ROOT_RUNNER_B;
+    char* ROOT_RUNNER_C;
+    char* ROOT_RUNNER_D;
 
-    char CRYSTAL_GOLEM_A;
-    char CRYSTAL_GOLEM_B;
-    char CRYSTAL_GOLEM_C;
-    char CRYSTAL_GOLEM_D;
+    char* CRYSTAL_GOLEM_A;
+    char* CRYSTAL_GOLEM_B;
+    char* CRYSTAL_GOLEM_C;
+    char* CRYSTAL_GOLEM_D;
 
-    char BLAZE_BREWER_A;
-    char BLAZE_BREWER_B;
-    char BLAZE_BREWER_C;
-    char BLAZE_BREWER_D;
+    char* BLAZE_BREWER_A;
+    char* BLAZE_BREWER_B;
+    char* BLAZE_BREWER_C;
+    char* BLAZE_BREWER_D;
 
-    char CONQUERTRON_A;
-    char CONQUERTRON_B;
-    char CONQUERTRON_C;
-    char CONQUERTRON_D;
+    char* CONQUERTRON_A;
+    char* CONQUERTRON_B;
+    char* CONQUERTRON_C;
+    char* CONQUERTRON_D;
 
-    char DRAGONET_A;
-    char DRAGONET_B;
-    char DRAGONET_C;
-    char DRAGONET_D;
+    char* DRAGONET_A;
+    char* DRAGONET_B;
+    char* DRAGONET_C;
+    char* DRAGONET_D;
 
-    char D_RIVETER_A;
-    char D_RIVETER_B;
-    char D_RIVETER_C;
-    char D_RIVETER_D;
+    char* D_RIVETER_A;
+    char* D_RIVETER_B;
+    char* D_RIVETER_C;
+    char* D_RIVETER_D;
 
 };
 
 struct Pieces initializeDeckPieces(struct Pieces);
 
 struct Pieces initializeBoardPieces(struct Pieces);
+
+struct Pieces initializeSpikes(struct Pieces);
 
 #endif
