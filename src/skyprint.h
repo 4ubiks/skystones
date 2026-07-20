f3cd74b0131a457574fd4ed3fd82f9a12c335b55
4ubiks <jaharris20@liberty.edu>
2026-07-20 12:24:01 -0400

    removed unused variables from function headers

--- src/skyprint.h
+++ src/skyprint.h
@@ -1,13 +1,11 @@
 void printBlankBoard();
 
-void printBoardPieces(struct Board, int turn, char* piecePlayed, struct Pieces *boardPieceCharacteristics);
+void printBoardPieces(struct Board board, char* piecePlayed, struct Pieces *boardPieceCharacteristics);
 
 void printBoardPieceContent(struct Board board);
 
 void printPieceCoordinates(int grid_x, int grid_y);
 
-void printPlayerDeck(struct Player player, struct Pieces deckPieces);
-
-void printPieceSpikes(WINDOW* spikesToPrint, char* itemToPrint);
+void printPlayerDeck(struct Player player);
 
 void enterPiece(int x, int y);
