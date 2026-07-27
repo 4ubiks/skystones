4d377d097da67953fa029ed3c91591641755b85b
4ubiks <jaharris20@liberty.edu>
2026-07-27 16:38:09 -0400

    adding null terminators to char assignments

--- src/pieces.c
+++ src/pieces.c
@@ -205,45 +205,45 @@ pieces = (struct Pieces){
 }
 
 struct Pieces initializeSpikes(struct Pieces pieces){
-    pieces.GRENADE_GENERAL_A = "0";
-    pieces.GRENADE_GENERAL_B = "2";
-    pieces.GRENADE_GENERAL_C = "0";
-    pieces.GRENADE_GENERAL_D = "2";
+    pieces.GRENADE_GENERAL_A = "0\0";
+    pieces.GRENADE_GENERAL_B = "2\0";
+    pieces.GRENADE_GENERAL_C = "0\0";
+    pieces.GRENADE_GENERAL_D = "2\0";
 
-    pieces.ARKEYAN_BOMBER_A = "2";
-    pieces.ARKEYAN_BOMBER_B = "2";
-    pieces.ARKEYAN_BOMBER_C = "2";
-    pieces.ARKEYAN_BOMBER_D = "0";
+    pieces.ARKEYAN_BOMBER_A = "2\0";
+    pieces.ARKEYAN_BOMBER_B = "2\0";
+    pieces.ARKEYAN_BOMBER_C = "2\0";
+    pieces.ARKEYAN_BOMBER_D = "0\0";
 
-    pieces.ROOT_RUNNER_A = "1";
-    pieces.ROOT_RUNNER_B = "2";
-    pieces.ROOT_RUNNER_C = "3";
-    pieces.ROOT_RUNNER_D = "2";
+    pieces.ROOT_RUNNER_A = "1\0";
+    pieces.ROOT_RUNNER_B = "2\0";
+    pieces.ROOT_RUNNER_C = "3\0";
+    pieces.ROOT_RUNNER_D = "2\0";
 
-    pieces.CRYSTAL_GOLEM_A = "3";
-    pieces.CRYSTAL_GOLEM_B = "3";
-    pieces.CRYSTAL_GOLEM_C = "0";
-    pieces.CRYSTAL_GOLEM_D = "0";
+    pieces.CRYSTAL_GOLEM_A = "3\0";
+    pieces.CRYSTAL_GOLEM_B = "3\0";
+    pieces.CRYSTAL_GOLEM_C = "0\0";
+    pieces.CRYSTAL_GOLEM_D = "0\0";
 
-    pieces.BLAZE_BREWER_A = "2";
-    pieces.BLAZE_BREWER_B = "0";
-    pieces.BLAZE_BREWER_C = "0";
-    pieces.BLAZE_BREWER_D = "2";
+    pieces.BLAZE_BREWER_A = "2\0";
+    pieces.BLAZE_BREWER_B = "0\0";
+    pieces.BLAZE_BREWER_C = "0\0";
+    pieces.BLAZE_BREWER_D = "2\0";
 
-    pieces.CONQUERTRON_A = "4";
-    pieces.CONQUERTRON_B = "4";
-    pieces.CONQUERTRON_C = "4";
-    pieces.CONQUERTRON_D = "4";
+    pieces.CONQUERTRON_A = "4\0";
+    pieces.CONQUERTRON_B = "4\0";
+    pieces.CONQUERTRON_C = "4\0";
+    pieces.CONQUERTRON_D = "4\0";
 
-    pieces.DRAGONET_A = "1";
-    pieces.DRAGONET_B = "2";
-    pieces.DRAGONET_C = "1";
-    pieces.DRAGONET_D = "2";
+    pieces.DRAGONET_A = "1\0";
+    pieces.DRAGONET_B = "2\0";
+    pieces.DRAGONET_C = "1\0";
+    pieces.DRAGONET_D = "2\0";
 
-    pieces.D_RIVETER_A = "2";
-    pieces.D_RIVETER_B = "0";
-    pieces.D_RIVETER_C = "2";
-    pieces.D_RIVETER_D = "0";
+    pieces.D_RIVETER_A = "2\0";
+    pieces.D_RIVETER_B = "0\0";
+    pieces.D_RIVETER_C = "2\0";
+    pieces.D_RIVETER_D = "0\0";
 
     return pieces;
 }
