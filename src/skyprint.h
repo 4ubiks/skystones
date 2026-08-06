d4a22d216c7307926d0000616b27c52164d3ce33
4ubiks <jaharris20@liberty.edu>
2026-08-06 17:43:20 -0400

    print spikes functions

--- src/skyprint.h
+++ src/skyprint.h
@@ -7,3 +7,12 @@ void printPlayerDeck(struct Player player, struct PieceInfo pieceSpikeInfo, int 
 void erasePieces();
 
 void printGridPieceOnSelection(int pieceSelection, WINDOW* gridSelectionWindow, struct PieceInfo pieceSpikes);
+
+void printTopSpikes(int numberOfSpikes, int x_spike, int y_spike, WINDOW* piece);
+
+void printRightSpikes(int numberOfSpikes, int x_spike, int y_spike, WINDOW* piece);
+
+void printBottomSpikes(int numberOfSpikes, int x_spike, int y_spike, WINDOW* piece);
+
+void printLeftSpikes(int numberOfSpikes, int x_spike, int y_spike, WINDOW* piece);
+
